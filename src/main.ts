fb9216508a98b2cf092daa4a1ece8fa2bccd9b4b
Tomáš Novotný <git@ontos.slmail.me>
2026-03-18 18:47:02 +0100

    some progress

--- src/main.ts
+++ src/main.ts
@@ -6,73 +6,133 @@ if (context === null) {
 const ctx = context as CanvasRenderingContext2D;
 
 
-const HEIGHT = canvas.height;
-const WIDTH = canvas.width;
-const LIGHT_SPEED = 1; // 1 pixel
+const SCREEN_HEIGHT: number = canvas.height;
+const SCREEN_WIDTH: number  = canvas.width;
+const SIM_HEIGHT: number = 75000000000.0; // in meters
+const SIM_WIDTH: number = 100000000000.0; // in meters
+const C: number = 2.99792458e8; // photon speed
+const G: number  = 6.67430e-11;
+const SOLAR_MASS = 1.989e30;  // kg
+const SAGITTARIUS_A_MASS = 4.3e6 * SOLAR_MASS;
+
+type Vector2 = {
+    x: number;
+    y: number;
+}
+type Position2 = Vector2;
+// lambda function
+const vec2 = (x: number, y: number) => ({x, y} satisfies Vector2)
+const pos2 = vec2
+
+// except Ray and BlackHole, use struct / function programming (for 3d use purely)
+
+class BlackHole {
+    public pos: Vector2;
+    public mass: number
+    public schwarzschildRadius: number
+    public gravity: number
+
+    public r: number
+    public color: string
+
+    constructor(pos: Vector2) {
+        this.pos = pos;
+        this.mass = SAGITTARIUS_A_MASS;
+        this.schwarzschildRadius = 2.0 * G * this.mass / C**2;
+        this.gravity = G * this.mass;
+
+        this.r = 70; // radius
+        this.color = "red";
+
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.stroke();;
+    }
+}
+const b = new BlackHole(pos2(0, 0));
 
 class Ray {
-    // coords
-    x: number = 10;
-    y: number = 10;
-    size: number = 2;
+    // cartesian
+    pos: Vector2;
+    dir: Vector2;
     // polar coords
-    r: number = Math.hypot(this.x, this.y);
-    phi: number = Math.atan2(this.y, this.x);
+    r: number;
+    phi: number;
+    dr: number;
+    dphi: number;
+    // conserved quantities
+    E: number;
+    L: number;
 
-    vector: [number, number] = [1, 0];
-    trail: [number, number][] = []; // array of tuples
+    // draw
+    trail: Position2[] = []; // array of pos history
+    size: number = 1;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+    constructor(position: Vector2, direction: Vector2) {
+        // cartesian
+        this.pos = position;
+        this.dir = direction;
+        // polar coords
+        this.r = Math.hypot(this.pos.x, this.pos.y);
+        this.phi = Math.atan2(this.pos.y, this.pos.x);
+        // seed velocities
+        this.dr = this.dir.x * Math.cos(this.phi) + this.dir.y * Math.sin(this.phi); // m / s
+        this.dphi = (this.dir.x * Math.sin(this.phi) + this.dir.y * Math.cos(this.phi)) / this.r;
+        // store conserved quantities
+        this.L = this.r**2 * this.dphi;
+        const f: number = 1.0 - b.schwarzschildRadius / this.r;
+        const dt_dλ: number = Math.sqrt((this.dr ** 2) / (f**2) + (this.r**2*this.dphi**2) / f);
+        this.E = f * dt_dλ;
+        // start trail
+        this.trail.push(this.pos);
     }
 
-    getPosBasedOnSize(): [number, number, number] {   
-        // canvas draw rect from corner, therefore this function usecase to account for it
-        return [this.x - (this.size / 2), this.y - (this.size / 2), this.size];
+    step(b: BlackHole) {
+        // collision of two circles Ray x BlackHole:
+        //const d = Math.hypot(this.pos.x - b.pos.x, this.pos.y - b.pos.y);
+        //if (d <= (this.size + b.r)) { // true if collision
+        //    return;
+        //}
+        //this.pos.x += this.dir.x;
+        //this.pos.y += this.dir.y;
+        //this.trail.push(this.pos)
+
+        const dλ: number = 1.0;
+        const rs: number = b.schwarzschildRadius;
+
+        // integrate (r,φ,dr,dφ)
+        if (this.r <= b.schwarzschildRadius) return; // stop if inside the event horizon
+
+        //rk4Step
+
+
+
+        //const x = this.r * Math.cos(this.phi);
+        //const y = this.r * Math.sin(this.phi);
+
+        // record the trail
+        this.trail.push(this.pos);
     }
 
-    getPolarCoords (): [number, number] {
-        //this.r = Math.hypot(this.x, this.y);
-        //this.phi = Math.atan2(this.y, this.x);
-        return [this.r, this.phi];
+
+    draw() {
+        const x = this.pos.x - (this.size / 2);
+        const y = this.pos.y - (this.size / 2);
+        const s = this.size
+
+        const stepLightIncrement: number = 1 / this.trail.length;
+        var stepLight: number = stepLightIncrement;
     }
-
-    step(b_r: number) {
-        // collision
-        if (this.r < b_r) {
-            return
-        }
-        this.x += this.vector[0];
-        this.y += this.vector[1];
-        this.trail.push([this.x, this.y])
-    }
-
-
-
 }
-
-const BlackHole = {
-    x: canvas.width * 0.8,
-    y: canvas.height * 0.5,
-    g: 9.8, // gravity
-    r: 70, // radius
-    color: "red"
-};
-const b = BlackHole;
-
-
-ctx.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
-ctx.fillStyle = b.color;
-ctx.fill();
-ctx.stroke();
-
-
-// project light
+// init Rays
 const projectedLight: Ray[] = [];
 const ArrayDistance = 70
-for (var i: number = 1; i < HEIGHT / ArrayDistance; i++) {
-    projectedLight.push(new Ray(100, i * ArrayDistance))
+for (var i: number = 1; i < SCREEN_HEIGHT / ArrayDistance; i++) {
+    projectedLight.push(new Ray(pos2(100, i * ArrayDistance), vec2(0, 0)))
 }
 
 
@@ -81,44 +141,30 @@ const fps: number = 60;
 const interval: number = 1500 / fps;
 
 function animate(timestamp: number) {
+    requestAnimationFrame(animate);
+
+    if (!lastTime) lastTime = timestamp;
     const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
 
-
-    ctx.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
-    ctx.fillStyle = b.color;
-    ctx.fill();
-    ctx.stroke();
+    b.draw(ctx);
 
     if (deltaTime > interval) {
         // Clear the canvas
 
-        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
         
         // light projectiles
         for (const l of projectedLight) {
-            const [x, y, s] = l.getPosBasedOnSize();
-
-            // stop wasting compute protection
-            if (Math.abs(x) > WIDTH * 10 || Math.abs(y) > HEIGHT * 10) {
-                continue;
-            }
-
-            l.step(b.r);
-
-            // draw whole tail
-            const stepLightIncrement: number = 1 / l.trail.length;
-            var stepLight: number = stepLightIncrement;
-            for (const [x, y] of l.trail) {
-                ctx.fillStyle = `rgba(255, 255, 255, ${stepLight})`;
-                ctx.fillRect(x, y, s, s);
-                stepLight += stepLightIncrement;
+                l.step(b);
+                l.draw();
             }
         }
-    }
     
     lastTime = timestamp - (deltaTime % interval);
     requestAnimationFrame(animate);
 }
+
 
 // Start the animation
 requestAnimationFrame(animate);
